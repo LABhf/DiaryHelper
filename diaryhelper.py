a8a62c4c046883c19152aaf0979d2d9e7b4f3fe3
LAB SD <28992896+LABhf@users.noreply.github.com>
2017-07-24 20:33:02 -0300

    Update diaryhelper.py

--- diaryhelper.py
+++ diaryhelper.py
@@ -1,28 +1,30 @@
+import os           #operating system dependent functionality
+import csv
+
+
 # diaryhelper asks questions about your day in a regular interval so that you don't forget about writing your diary
 
         #MENU
             #ALTER QUESTIONS
                 #ADD QUESTIONS
                 #REMOVE QUESTIONS
+
+q={'janta': '21', 'almoco': 14, 'geral': '22'}
+#q={}
+localq=q
+
+
 def mainbody():
                 #READ DIARY FILE
     print("     MENU")
     print("---")
     print("[1] ADD QUESTIONS")
     print("[2] REMOVE QUESTIONS")
-    print("[3] READ DIARY FILE")
+    print("[3] UPDATE CONF. FILE")
+                
     print("---")
 
 
-    #dict = {'Almoço': '15' , 'janta': '21' }
-    #print(dict)
-    #print(dict["janta"])
-    #for a in dict:
-    #    print(a)
-
-    q={}
-    #for debug >
-    #q={'janta': '21', 'almoco': 14, 'geral': '22'}      
     x=55
 
 
@@ -51,6 +53,13 @@ def mainbody():
                 more = input(": ")
                 more = int(more)
 
+                # Essa solução não parece ser boa, mas é um jeito de acessar a q local do addq
+                # e usar ela em uma var global localq, a sintaze tbm ta confusa...
+
+                global localq
+                localq = q
+                print(localq, q)
+
                 if more == 1:
                     print("## MORE 1")
                     addq()
@@ -59,15 +68,13 @@ def mainbody():
 
             addq()
 
-            if more == 1:
-                addq()
         elif a == 2:
 
             def rmq():
                 print("WICH QUESTIONS YOU WANT TO REMOVE?")
                 i = 0
                 ll=[]
-                for a in q:
+                for a in localq:
                     i=i+1
                     print(i,".", a)
                     ll.append(a)
@@ -77,17 +84,34 @@ def mainbody():
                 rname = int(rname)
                 rname = rname - 1
                 ff=str(ll[rname])
-                del q[ff]
+                del localq[ff]
                 del ll[rname]
                 print(ll)
 
                 mainbody()
             rmq()
         elif a == 3:
-            print("#3")
-            #read doc
+            print("#3 UPDATE")
+            #UPDATE
 
-        else:
+
+
+            def w(q):
+                with open("data.csv", "w") as f:
+                    #f.write(str(print(a, localq[a])))
+                    print(localq)
+                    for a in localq:
+                        f.write(str(a))
+                        f.write(":")
+                        f.write(str(localq[a]))
+                        f.write(",")
+
+            w(localq)
+
+            print("#3 FINAL")
+
+
+        else:           # This is answer filter...
             print("Choose a number between 1 and 3")
             #repeat
             answer(a)
