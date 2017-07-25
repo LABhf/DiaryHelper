1090423f27082e5d7df54abad6b75f8f4cbc307c
LAB SD <28992896+LABhf@users.noreply.github.com>
2017-07-25 17:09:15 -0300

    Update diaryhelper.py
    finished and clean the code, but still with "problems" in the q and localq variables...

--- diaryhelper.py
+++ diaryhelper.py
@@ -1,38 +1,28 @@
-import os           #operating system dependent functionality
-import csv
-
-
 # diaryhelper asks questions about your day in a regular interval so that you don't forget about writing your diary
 
-        #MENU
-            #ALTER QUESTIONS
-                #ADD QUESTIONS
-                #REMOVE QUESTIONS
+# debuging q
+# q={'what did you eat today?': '21', 'what time did you get up?': '15', 'what did you do today?': '22'}
 
-q={'janta': '21', 'almoco': 14, 'geral': '22'}
-#q={}
-localq=q
-
-
+# normal q
+q={}
+localq = q
 def mainbody():
                 #READ DIARY FILE
     print("     MENU")
     print("---")
     print("[1] ADD QUESTIONS")
     print("[2] REMOVE QUESTIONS")
-    print("[3] UPDATE CONF. FILE")
-
+    print("[3] UPDATE AND EXIT")
     print("---")
 
-
+    # this is a trigger for the answer() since is
     x=55
-
 
     def answer(a):
         a = int(input("Choose: "))
 
         if a == 1:
-            print("#1")
+            # print("#1")
 
             def addq():
                 print('CURRENT QUESTIONS')
@@ -43,22 +33,47 @@ def mainbody():
 
                 qname = input("QUESTION NAME: ")
                 print(qname)
+
                 trigger= input("TRIGGER TIME: ")
                 print(trigger)
+
+                if trigger.isdigit() == False:
+                    print("TRIGGER NOT AN INTEGER")
+                    print("")
+                    addq()
+
+                else:
+                    if int(trigger) < 1 or int(trigger) > 24:
+                        print("TRIGGER SHOULD BE 1 TO 24")
+                        addq()
+                    else:
+                        pass
 
                 q.update({qname: trigger})
 
                 print(q)
                 print("MORE QUESTIONS?   YES=1,   NO =0 ")
+
                 more = input(": ")
+
+                if more.isdigit() == False:
+                    print("NOT AN INTEGER SHOULD BE 1 OR 0")
+                    print("")
+                    more=""
+                    addq()
+                else:
+                    pass
+
                 more = int(more)
 
                 # Essa solução não parece ser boa, mas é um jeito de acessar a q local do addq
                 # e usar ela em uma var global localq, a sintaze tbm ta confusa...
-
+                 
+                # o problema aqui é que eu tenho que citar localq =q aqui e 
+                # no inicio do programa, pra poder executar o elif == 3 
+                
                 global localq
                 localq = q
-                print(localq, q)
 
                 if more == 1:
                     print("## MORE 1")
@@ -71,16 +86,23 @@ def mainbody():
         elif a == 2:
 
             def rmq():
+                if localq=={}:
+                    print("NO QUESTIONS ... ")
+                    mainbody()
+                else:
+                    pass
+
                 print("WICH QUESTIONS YOU WANT TO REMOVE?")
                 i = 0
                 ll=[]
+                # print(localq)
                 for a in localq:
                     i=i+1
-                    print(i,".", a)
+                    print(i,".", a, ":", localq[a], "HOURS")
                     ll.append(a)
                     ####
-
-                rname = input(": ")
+                print("")
+                rname = input("CHOSE A NUMBER:  ")
                 rname = int(rname)
                 rname = rname - 1
                 ff=str(ll[rname])
@@ -91,16 +113,13 @@ def mainbody():
                 mainbody()
             rmq()
         elif a == 3:
-            print("#3 UPDATE")
-            #UPDATE
 
-
-            #esse jeito é um pouco mais simples pra passar pro data.csv, mas a sintaxe é bem estranha pra transformar o dado no
-            # bgprocess, que é o mesmo arquivo que o ,reader debug, ...
-            def w(q):
+            def w(val):
                 with open("data.csv", "w") as f:
                     #f.write(str(print(a, localq[a])))
+                    print("CURRENT CONFIG FILE:")
                     print(localq)
+                    print("")
                     for a in localq:
                         f.write(str(a))
                         f.write(":")
@@ -109,18 +128,19 @@ def mainbody():
 
             w(localq)
 
-            print("#3 FINAL")
 
 
-        else:           # This is answer filter...
+        # This is answer filter...
+        else:
             print("Choose a number between 1 and 3")
             #repeat
             answer(a)
 
+
     answer(x)
 
 if __name__ == '__main__':
-    print("#T")      #isso só vai rodar se for executado diretamente o arquivo mymath
+
     mainbody()
 
-print("#EXIT")
+print("EXIT")
