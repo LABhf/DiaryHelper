b62615d98ab69c6489c11bfd3b23173f993168ec
LAB SD <28992896+LABhf@users.noreply.github.com>
2017-07-28 16:06:13 -0300

    Update warn.py

--- warn.py
+++ warn.py
@@ -1,2 +1,3 @@
-#this file should come after the bgprocess to show the questions in the time defined in trigger
+#this file should come after the bgprocess to show the questions in the time defined in trigger from the data2.csv temporary file
 #it should not open more than once for the same question
+#it should open a prompt ask the question, retrieve info and add it to the major diary.txt file.
